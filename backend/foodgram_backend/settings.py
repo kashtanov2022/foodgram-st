9b9bfc1aee9b66c01da7ee29e529af249d68aa7d
Invisi <kashtanov.2022.2022@gmail.com>
2025-05-31 15:36:13 +0700

    Docker containerization is implemented

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -122,11 +122,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -186,7 +181,7 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Раскомментируйте для продакшена
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_volume')
 
 # Если будете использовать кастомную модель пользователя
 # AUTH_USER_MODEL = 'users.User' # Укажем это позже, когда создадим приложение users и модель
